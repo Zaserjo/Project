d5bfa5f475cb5312edf273495f7eb64f94ab2861
Talha Anwar <ANW16888@spengergasse.at>
2016-02-16 10:41:01 +0100

    Ich mache:
    -) Graphisches Interface

--- src/Project/Test.java
+++ src/Project/Test.java
@@ -6,7 +6,16 @@ public class Test
     public static void main(String[] args)
     {
         System.out.println("Hello Mani");
-
+        /**
+         * Manuel:
+         * Fragen in Arrays
+         * Statistik
+         * 
+         * 
+         * Talha:
+         * Graphische Interface
+         * Spielerprofil  
+         * Dia       
+         **/
     }
-
 }
